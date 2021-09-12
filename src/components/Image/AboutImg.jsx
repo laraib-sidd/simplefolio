08786d9feb7aaf93866d920e4049d0f6f935b670
Laraib siddiqui <34001535+laraib-sidd@users.noreply.github.com>
2021-09-12 14:50:43 +0530

    Update AboutImg.jsx

--- src/components/Image/AboutImg.jsx
+++ src/components/Image/AboutImg.jsx
@@ -12,7 +12,7 @@ const AboutImg = ({ filename, alt }) => (
             node {
               relativePath
               name
-              childImageSharp {fixed(width: 1080) {
+              childImageSharp {fixed(width: 1440) {
                   ...GatsbyImageSharpFixed
                 }
               }
