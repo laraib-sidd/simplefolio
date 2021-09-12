8e1f3bdd416021faaa0258fb1ca5a3c90b59cb61
Laraib siddiqui <34001535+laraib-sidd@users.noreply.github.com>
2021-09-12 15:16:12 +0530

    Update AboutImg.jsx

--- src/components/Image/AboutImg.jsx
+++ src/components/Image/AboutImg.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
-
 const AboutImg = ({ filename, alt }) => (
   <StaticQuery
     query={graphql`
@@ -13,7 +12,7 @@ const AboutImg = ({ filename, alt }) => (
               relativePath
               name
               childImageSharp {
-                fixed(width: 1366) {
+                fixed(width: 350) {
                   ...GatsbyImageSharpFixed
                 }
               }
@@ -24,18 +23,13 @@ const AboutImg = ({ filename, alt }) => (
     `}
     render={(data) => {
       const image = data.images.edges.find((n) => n.node.relativePath.includes(filename));
-
       if (!image) return null;
-
-      const imageFixed = image.node.childImageSharp.fluid;
+      const imageFixed = image.node.childImageSharp.fixed;
       return <Img className="rounded shadow-lg" alt={alt} fixed={imageFixed} />;
     }}
   />
 );
-
 AboutImg.propTypes = {
   filename: PropTypes.string,
   alt: PropTypes.string,
 };
-
-export default AboutImg;
